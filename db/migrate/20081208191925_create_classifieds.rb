0c19a769923a145d58a86d1544e40206cd4fecc6
Benjamin Fritsch <beanie@benle.de>
2008-12-08 20:24:39 +0100

    skeleton for classifieds

---
+++ db/migrate/20081208191925_create_classifieds.rb
@@ -0,0 +1,14 @@
+class CreateClassifieds < ActiveRecord::Migration
+  def self.up
+    create_table :classifieds do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :classifieds
+  end
+end
