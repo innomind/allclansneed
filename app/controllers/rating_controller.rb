9561fa029d34b84fd2b6ea5ed4faa4ea50518445
Philipp Wesner <pwesner@innomind.info>
2009-03-04 18:08:39 +0100

    minor rating changes

--- app/controllers/rating_controller.rb
+++ app/controllers/rating_controller.rb
@@ -1,9 +1,12 @@
 class RatingController < ApplicationController
   def rate
-    @asset = Article.find params[:id]
-    Rating.delete_all(["rateable_type = 'Article' AND rateable_id = ? AND user_id = ?", @asset.id, current_user.id])
-    @asset.add_rating Rating.new(:rating => params[:rating], :user_id => current_user.id)
+    begin
+      @asset = eval(params[:asset_name]).find params[:id]
+      Rating.delete_all(["rateable_type = 'Article' AND rateable_id = ? AND user_id = ?", @asset.id, current_user.id])
+      @asset.add_rating Rating.new(:rating => params[:rating], :user_id => current_user.id)
+    rescue NameError
+      render :layout => true, :text => "ups" and return
+    end
   end
-
 end
 
