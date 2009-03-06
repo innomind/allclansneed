1407df8bbc842c0eb287a47a3fea9f5c878a4ae6
Philipp Moeller <bootsarehax@googlemail.com>
2009-03-06 16:13:42 +0100

    minor rating change again
    still some hard coded string in controller

--- app/controllers/rating_controller.rb
+++ app/controllers/rating_controller.rb
@@ -2,7 +2,7 @@ class RatingController < ApplicationController
   def rate
     begin
       @asset = eval(params[:asset_name]).find params[:id]
-      Rating.delete_all(["rateable_type = 'Article' AND rateable_id = ? AND user_id = ?", @asset.id, current_user.id])
+      Rating.delete_all(["rateable_type = '" + params[:asset_name].to_s + "' AND rateable_id = ? AND user_id = ?", @asset.id, current_user.id])
       @asset.add_rating Rating.new(:rating => params[:rating], :user_id => current_user.id)
     rescue NameError
       render :layout => true, :text => "ups" and return
