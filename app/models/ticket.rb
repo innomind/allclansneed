c6923e6bceee6536885802d9d6628eba4fca62a0
Benjamin Fritsch <beanie@benle.de>
2009-03-12 17:13:21 +0100

    ticketsystem

---
+++ app/models/ticket.rb
@@ -0,0 +1,12 @@
+class Ticket < ActiveRecord::Base
+  belongs_to :admin, :class_name  => "User", :foreign_key  => "admin_id"
+  belongs_to :author, :class_name => "User", :foreign_key => "author_id"
+  
+  has_many :ticket_messages, :dependent => :destroy
+  
+  validates_presence_of :author_id
+  
+  def ticket_message=(ticket_message)
+    ticket_messages.build(ticket_message)
+  end
+end
