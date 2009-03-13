1af8f6a2d4c99a270ae6bc556065513fe11b7ad9
Benjamin Fritsch <beanie@benle.de>
2009-03-13 22:05:52 +0100

    added daemon plugin

--- lib/autodns.rb
+++ lib/autodns.rb
@@ -1,5 +1,3 @@
-#!/usr/bin/ruby
-
 require 'net/https'
 require 'net/http'
 require 'rexml/document'
@@ -7,18 +5,16 @@ require "uri"
 include REXML
 
 class Autodns
-  
-  def initialize
+  @@document = nil
+  def initialize (task, domain)
     #Creating XML-Skeleton
-    document = Document.new
-      document.add_element("request")
-      
-    build_auth_tag document
-    build_task_tag document
-    request document
+    @document = Document.new
+      @document.add_element("request")
+    build_auth_tag
+    build_task_tag task, domain 
   end
   
-  def build_auth_tag document
+  def build_auth_tag
     #Building authorization tag
     auth = Element.new("auth")
       auth.add_element("user")
@@ -30,28 +26,35 @@ class Autodns
       auth.elements["context"].text = "4"
       
       #adding authorization-tag to document
-      document.elements["request"].elements << auth
+      @document.elements["request"].elements << auth
   end
   
-  def build_task_tag document
-    #building some tasks
-    task = Element.new("task")
-    task.add_element("code")
-    task.elements["code"].text = "0105"
-  
-    domain = Element.new("domain")
-    domain.add_element("name")
-    domain.elements["name"].text = "innomind.info"
+  def build_task_tag (task, domain)
+    
+    case task
+    when (task == :check) then
+      #building domain check
+      task = Element.new("task")
+      task.add_element("code")
+      task.elements["code"].text = "0105"
 
-    task.elements << domain
-    document.elements["request"].elements << task
+      domain = Element.new("domain")
+      domain.add_element("name")
+      domain.elements["name"].text = domain
+
+      task.elements << domain
+    when (task == :register) then
+      #building domain check
+    end
+    
+    @document.elements["request"].elements << task
   end
   
-  def request document
+  def request
     @http = Net::HTTP.new('gateway.autodns3.de', 443)
     @http.use_ssl = true
     @http.start() {|http|
-      resp, data = http.post('/', document.to_s)
+      resp, data = http.post('/', @document.to_s)
       return resp.body
     }
   end
