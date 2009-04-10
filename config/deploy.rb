ea873eb9142b5c19f6a379d5b96d0e29bb16f9e1
Philipp Wesner <pwesner@innomind.info>
2009-04-10 16:18:01 +0200

    asdfasdfasdföahds föausdh flausdhf laisudfh

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,15 +8,6 @@ role :app, "dev.innomind.info"
 role :web, "dev.innomind.info"
 role :db, "dev.innomind.info", :primary => true
 
-set :user, "root"
+set :user, "pw"
 set :use_sudo, false
 set :deploy_via, :remote_cache
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
-
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
