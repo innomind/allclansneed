b0e67ca036982d824a0b26699308de9c05471040
Philipp Wesner <pwesner@innomind.info>
2009-04-16 20:41:33 +0200

    group, clan, user, lists styling improved

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,5 +1,5 @@
 module CategoriesHelper
   def categories_link controller, link_text = "Kategorien bearbeiten", options = {} 
-    link_to image_tag("folder.png") + " " + link_text, category_path(controller) if @user.has_right_for? controller.underscore unless @user.nil?
+    link_to image_tag("folder.png") + link_text, category_path(controller) if @user.has_right_for? controller.underscore unless @user.nil?
   end
 end
