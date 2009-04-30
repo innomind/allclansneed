c7acbe125855cc7a2c06f6129a3964cc68a99f4e
Philipp Wesner <pwesner@innomind.info>
2009-04-30 13:29:19 +0200

    gallery pic asset host bugfix

--- app/helpers/gallery_pic_helper.rb
+++ app/helpers/gallery_pic_helper.rb
@@ -1,11 +1,11 @@
 module GalleryPicHelper
 
   def gallery_pic_link_for pic
-    if ActionController::Base.asset_host.empty?
-  		link_to image_tag(@gallery_pic.pic.url(:medium)), @gallery_pic.pic.url(:original)
-  	else
+    #if ActionController::Base.asset_host.empty?
+  	#	link_to image_tag(@gallery_pic.pic.url(:medium)), @gallery_pic.pic.url(:original)
+  	#else
   		"<a href=\"http://static0.allclansneed.de#{@gallery_pic.pic.url(:original)}\">#{image_tag(@gallery_pic.pic.url(:medium))}</a>"
-  	end
+  	#end
   end
 
 end
